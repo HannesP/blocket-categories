3284c8fe8f7e4c7f8ba5f5d63b3b0edeaf72761e
Hannes Petri <mail@hannesp.se>
2018-03-03 12:12:43 +0100

    Now handles case 1 (Skåne+Norrbotten), case 2 (sub-areas in Sthlm and Gbg) and case 3 (the rest)

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ def get_num(mun, cat_no):
 	else:
 		mun_no1, mun_no2 = mun
 		params = {'mun_no1': mun_no1, 'mun_no2': mun_no2, 'cat_no': cat_no}
-		url_temp = 'https://www.blocket.se/hela_sverige?q=&cg={cat_no}&w=1&m={mun_no1}&st=s&cs=&ck=&csz=&f=p&ca={mun_no2}&is=1&l=0&md=th'
-		
+		if '_' in mun_no1:
+			url_temp = 'https://www.blocket.se/stockholm?q=&cg={cat_no}&w=1&as={mun_no1}&st=s&ps=&pe=&c=&ca={mun_no2}&is=1&l=0&md=th&f=p'
+		else:
+			url_temp = 'https://www.blocket.se/hela_sverige?q=&cg={cat_no}&w=1&m={mun_no1}&st=s&cs=&ck=&csz=&f=p&ca={mun_no2}&is=1&l=0&md=th'
+
 	url = url_temp.format(**params)
+	print(url)
 	res = requests.get(url)
 	html = res.text
 	m = re.search(r'"num_hits">(.*?)<', html)
@@ -25,14 +29,28 @@ def get_vector(mun):
 	return [x/sum(vec) for x in vec]
 
 def main():
-	res = {}
+	filename = 'res.json'
+	try:
+		res = json.load(open(filename))
+	except FileNotFoundError:
+		res = {}
 	
 	for mun_name, mun in municipalities.items():
-		print(mun_name)
-		vec = get_vector(mun)
-		res[mun_name] = vec
-		print('{}\n'.format([round(x,2) for x in vec]))
-
+		if mun_name in res:
+			continue
+		
+		try:
+			vec = get_vector(mun)
+			res[mun_name] = vec
+			json.dump(res, open(filename, 'w'), ensure_ascii=False)
+			
+			print(mun_name)
+			print('{}\n'.format([round(x,2) for x in vec]))
+		except Exception as e:
+			print('ERROR!')
+			print(mun_name)
+			print(mun)
+		
 	out = json.dumps(res, sort_keys=True, indent=4)
 	print(out)
 

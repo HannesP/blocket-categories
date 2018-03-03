e38e024f774c2448ebcab2cbe0a7cbb3421c9719
Hannes Petri <mail@hannesp.se>
2018-03-03 12:13:18 +0100

    Added sample result

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ import json
 from plotly.graph_objs import Scatter, Figure, Layout
 from plotly.offline import plot
 
-data = json.load(open('skåne.json'))
+data = json.load(open('res.json'))
 mat = list(data.values())
 
 mds = sklearn.manifold.MDS()
@@ -14,7 +14,10 @@ trace = Scatter(
 	x=fit[:,0],
 	y=fit[:,1],
 	mode='markers',
-	text=list(data.keys())
+	text=list(data.keys()),
 )
+
+#layout = Layout(hovermode= 'closest')
+#figure = Figure(data=trace)
 
 plot([trace])
